f62c78177642cd6726c95d502c230d83ffe73613
Yogesh Lakhani <ylakhani39@gmail.com>
2022-10-04 17:24:03 +0300

    new-push

--- js/validate.js
+++ js/validate.js
@@ -1,9 +1,8 @@
 const form = document.getElementById('regForm');
-const patientName = document.getElementById('floatingInput');
-const NinFields = document.getElementById('Input');
-const regDate = document.getElementById('date-pick');
-
-
+let txtinput = document.querySelectorAll('.form-control');
+let tblsel = document.querySelectorAll('.form-check-input');
+const select = document.getElementById('select');
+let gender = document.querySelectorAll('.radio1');
 
 
 form.addEventListener('submit', e => {
@@ -15,28 +14,50 @@ form.addEventListener('submit', e => {
 
 const checkInputs = () => {
 
-	if(patientName.value === ""){
-	patientName.style.border = '2px solid red';
-	patientName.focus();
-	}else{
-	patientName.style.border = '2px solid green';
-	patientName.focus();
-	}
+	// for input fields
+    for(var i = 0; i < txtinput.length; i++){
+        if(txtinput[i].value === ""){
+           txtinput[i].style.border = '4px solid red';
+            txtinput[i].focus();
+        }else{
+            txtinput[i].style.border = '4px solid green';
+            txtinput[i].focus();
+        }
+    }
 
-	if(NinFields.value ===""){
-		NinFields.style.border = '2px solid red';
-		NinFields.focus();
-	}else{
-		NinFields.style.border = '2px solid green';
-		NinFields.focus();
-	}
+    // for table
+    for(var j = 0; j < tblsel.length; j++){
+        if(tblsel[j].checked === true){
+            tblsel[j].style.border= '4px solid green';
+            tblsel[j].focus();
+        }
+    }
 
-	if(regDate.value ===""){
-		regDate.style.border = '2px solid red';
-		regDate.focus();
-	}else{
-		regDate.style.border = '2px solid green';
-		regDate.focus();
-	}
+   
+//   select region
+    for(var k =0; k<select.length; k++){
 
+        if(select.innerText === try1){
+            select.style.border = '2px solid red';
+            select.focus();
+        }else{
+            select.style.border = '2px solid green';
+            select.focus();
+        }
+    }
+  
+
+    // radio btn
+    for(var z=0; z < gender.length; z++){
+
+        if(gender[z].selected === false){
+            gender[z].style.border = '4px solid red';
+            gender[z].focus();
+        }else{
+            gender[z].style.border = '4px solid green';
+            gender[z].focus();
+        }
+    }
 }
+
+
